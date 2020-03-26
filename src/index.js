db0c12fd46951b7283984309585fce846792a791
Felipe Freitag Vargas <ffvargas@gmail.com>
2020-03-26 09:30:29 -0300

    Personal preferences

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import Phaser from "phaser";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
-import playGame from "./phaser/scene";
-
-//console.log(App);
+import Phaser from 'phaser'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import playGame from './phaser/scene'
 
 export const config = {
   type: Phaser.AUTO,
-  parent: "phaser",
+  parent: 'phaser',
   width: 800,
   height: 600,
-  scene: playGame
-};
+  scene: playGame,
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
 
 ReactDOM.render(
   <App />,
-  document.getElementById("root") || document.createElement("div")
-);
+  document.getElementById('root') || document.createElement('div'),
+)
